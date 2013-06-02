6a173abaa35c18303dece7955d0c76e67dd19dba
Barbie <barbie@missbarbell.co.uk>
2013-06-02 08:02:21 +0000

    Import of BARBIE/Data-Phrasebook-Loader-Ini-0.13 from CPAN.
    gitpan-cpan-distribution: Data-Phrasebook-Loader-Ini
    gitpan-cpan-version:      0.13
    gitpan-cpan-path:         BARBIE/Data-Phrasebook-Loader-Ini-0.13.tar.gz
    gitpan-cpan-author:       BARBIE
    gitpan-cpan-maturity:     released

--- lib/Data/Phrasebook/Loader/Ini.pm
+++ lib/Data/Phrasebook/Loader/Ini.pm
@@ -5,7 +5,7 @@ use Carp qw( croak );
 use base qw( Data::Phrasebook::Loader::Base Data::Phrasebook::Debug );
 use Config::IniFiles;
 
-our $VERSION = '0.12';
+our $VERSION = '0.13';
 
 =head1 NAME
 
@@ -196,9 +196,9 @@ be forthcoming, please feel free to (politely) remind me.
 
 =head1 COPYRIGHT AND LICENSE
 
-  Copyright (C) 2004-2012 Barbie for Miss Barbell Productions.
+  Copyright (C) 2004-2013 Barbie for Miss Barbell Productions.
 
-  This module is free software; you can redistribute it and/or
+  This distribution is free software; you can redistribute it and/or
   modify it under the Artistic License 2.0.
 
 =cut
